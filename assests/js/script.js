e46d5fbc9be26fd2abeb412f1666f8c2853803fa
unknown <compilecoding@gmail.com>
2025-07-20 00:53:49 +0530

    1st commit

---
+++ assests/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#bar").addEventListener("click",function(){
+    document.querySelector(".mobile-header").style.right = "0"    
+})
+document.querySelector("#xmark").addEventListener("click",function(){
+    document.querySelector(".mobile-header").style.right = "-100%"    
+})
